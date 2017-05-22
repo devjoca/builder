ef4b19f5d5182fec99883db5d2732aad275bc454
Joca <jpereyraleon@gmail.com>
2017-05-21 20:43:02 -0500

    Fix button

--- resources/assets/js/components/BuildBtn.vue
+++ resources/assets/js/components/BuildBtn.vue
@@ -1,9 +1,9 @@
 <template>
-    <button class="btn" v-on:click="build" :disabled="isBuilding">{{ message }}</button>
+    <button class="btn" @click="build(projectId)" :disabled="isBuilding">{{ message }}</button>
 </template>
 <script>
-    module.exports = {
-        data: () => {
+    export default {
+        data() {
             return {
                 message: "Build!",
                 isBuilding: false
@@ -11,17 +11,16 @@
         },
         props: ['projectId'],
         methods: {
-            build: () => {
-                this.message = "... Building!";
-                this.isBuilding = true;
-                console.log(this.message);
+            build(projectId) {
+                const vm = this;
+                vm.message = "... Building!";
+                vm.isBuilding = true;
 
-                // axios.post(`projects/${id}/build`)
-                //     .then(function (response) {
-                //         this.isBuilding = true;
-                //         this.message = "Build!";
-                //         console.log(response);
-                //     });
+                axios.post(`projects/${projectId}/build`)
+                    .then(function (response) {
+                        vm.isBuilding = false;
+                        vm.message = "Build!";
+                    });
             }
         }
     }
